48f97b7c1698bd993042b08568d2fa7c95fdb47f
RZhurakovskiy <130534672+RZhurakovskiy@users.noreply.github.com>
2023-10-31 10:13:30 +0300

    Add files via upload

---
+++ TS-Contacts-01.ts
@@ -0,0 +1,31 @@
+{type Users = { name: string; age: number; group: string };
+
+const persons: Users[] = [
+    {
+        name: 'Иван Петров',
+        age: 27,
+        group: 'SEO-специалист',
+      },
+      {
+        name: 'Марат Aляуддинов',
+        age: 20,
+        group: 'Музыкант',
+      },
+      {
+        name: 'Анастасия Махнева',
+        age: 32,
+        group: 'Супруга',
+      },
+      {
+        name: 'Елизавета Махнева',
+        age: 12,
+        group: 'Дочь',
+      }
+];
+
+const logPerson = (user: Users) => {
+    console.log(`${user.name}, ${user.age}`);
+  }
+  
+  console.log('Users:');
+  persons.forEach(logPerson);}
